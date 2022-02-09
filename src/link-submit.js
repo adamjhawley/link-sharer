7325171257a2935df99e6af2b64009a363cb8af6
adamjhawley <adamhawley99@gmail.com>
2022-02-09 21:00:29 +0200

    link-submit: new component to submit new links
    Signed-off-by: adamjhawley <adamhawley99@gmail.com>

---
+++ src/link-submit.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {LitElement, html, css} from 'lit';
+import './link-list'
+
+/**
+ * The small box containing a link and a title.
+ */
+export class LinkSubmit extends LitElement {
+  static get styles() {
+    return css`
+    `;
+  }
+
+  static get properties() {
+    return {
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+    <sl-input label="Title" placeholder="Enter title here" clearable></sl-input>
+    <sl-input label="Link" placeholder="Enter link here" clearable></sl-input>
+    `;
+  }
+}
+
+window.customElements.define('ls-link-submit', LinkSubmit);
