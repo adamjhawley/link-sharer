7ae4f14be97fa005767281b749920d797bb89c83
adamjhawley <adamhawley99@gmail.com>
2022-04-01 21:29:23 +0300

    link-submit: connected link submission to Firestore
    Signed-off-by: adamjhawley <adamhawley99@gmail.com>

--- src/link-submit.js
+++ src/link-submit.js
@@ -1,36 +1,53 @@
 /**
  * @license
- * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html, css} from 'lit';
+import { LitElement, html } from 'lit';
 import './link-list'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from 'firebase/firestore/lite';
 
 /**
  * The small box containing a link and a title.
  */
 export class LinkSubmit extends LitElement {
-  static get styles() {
-    return css`
-    `;
-  }
-
   static get properties() {
     return {
+      user: {type: Object},
+      app: {type: Object}
     };
   }
 
-  constructor() {
-    super();
+  get _form () {
+    return this.renderRoot.querySelector('#submit-form')
+  }
+
+  async submitLink (e) {
+    let userTitle = this._form.querySelector('#title-input').value
+    let userLink = this._form.querySelector('#link-input').value
+    const db = getFirestore(this.app);
+    let col = collection(db, 'links')
+    await addDoc(col, {
+      link: userLink,
+      title: userTitle
+    })
+
+    const event = new CustomEvent('submitted-link')
+    this.dispatchEvent(event)
   }
 
   render() {
     return html`
-    <sl-input label="Title" placeholder="Enter title here" clearable></sl-input>
-    <sl-input label="Link" placeholder="Enter link here" clearable></sl-input>
-    <br>
-    <sl-button>Submit</sl-button>
+    <div id='submit-form'>
+      <sl-input id="title-input" label="Title" placeholder="Enter title here" clearable></sl-input>
+      <sl-input id="link-input" type="url" label="Link" placeholder="Enter link here" clearable></sl-input>
+      <br>
+      <sl-button @click=${this.submitLink}>Submit</sl-button>
+    </div>
     `;
   }
 }
